d5cb8dd9d640407c912e158b2c3b43b29c0f45b4
rbbozhilov <rbbojilov@gmail.com>
2022-04-05 20:13:20 +0300

    Add new project tests , and mock for database in memory

---
+++ LionSkyNot/LionSkyNot.Tests/Mock/DatabaseMock.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using LionSkyNot.Data;
+
+using Microsoft.EntityFrameworkCore;
+
+
+namespace LionSkyNot.Tests.Mock
+{
+    public static class DatabaseMock
+    {
+
+        public static LionSkyDbContext Instance
+        {
+            get
+            {
+                var dbContextOptions = new DbContextOptionsBuilder<LionSkyDbContext>()
+                    .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                    .Options;
+
+                return new LionSkyDbContext(dbContextOptions);
+            }
+        }
+
+    }
+}
