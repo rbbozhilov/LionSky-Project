0976c5acef18497d1607d13bfb6769cb99467378
rbbozhilov <rbbojilov@gmail.com>
2022-04-08 09:41:31 +0300

    Add integration tests on Recipe , Home controllers

---
+++ LionSkyNot/LionSkyNot.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+using LionSkyNot.Controllers;
+
+using LionSkyNot.Data.Models.Classes;
+
+using LionSkyNot.Models;
+
+using LionSkyNot.Services.Trainers;
+
+using LionSkyNot.Tests.Mock;
+
+using LionSkyNot.Views.ViewModels.Trainers;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Xunit;
+
+
+namespace LionSkyNot.Tests.Controllers
+{
+    public class HomeControllerTest
+    {
+
+        [Fact]
+        public void HomeIndex_ShouldReturnCorrectViewModelAndViewResult()
+        {
+
+            //Arrange
+
+            using var data = DatabaseMock.Instance;
+            var trainerService = new TrainerService(data);
+            var homeController = new HomeController(trainerService);
+
+            var trainer = new Trainer()
+            {
+                Id = 1,
+                Description = "description",
+                ImageUrl = "imageurl",
+                FullName = "Conor mcgregor",
+                UserId = "userid",
+                YearOfExperience = 5,
+                BirthDate = DateTime.Now,
+                Weight = 50,
+                Height = 170,
+                Categorie = new Categorie() { Id = 1, Name = "Box"}
+            };
+
+            data.Trainers.Add(trainer);
+            data.SaveChanges();
+
+            //Act
+
+            var currentTrainers = new List<TrainerViewModel>();
+
+            var viewResult = homeController.Index(currentTrainers);
+
+            //Assert
+
+            var viewModel = Assert.IsType<ViewResult>(viewResult);
+
+            var listOfTrainers = Assert.IsType<List<TrainerViewModel>>(viewModel.Model);
+
+            Assert.Equal(1, listOfTrainers.Count);
+
+        }
+
+    }
+}
---
+++ LionSkyNot/LionSkyNot.Tests/Controllers/RecipeControllerTest.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+
+using LionSkyNot.Controllers;
+
+using LionSkyNot.Data.Models.Recipe;
+
+using LionSkyNot.Services.Recipes;
+
+using LionSkyNot.Tests.Mock;
+
+using LionSkyNot.Views.ViewModels.Recipes;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Xunit;
+
+
+namespace LionSkyNot.Tests.Controllers
+{
+    public class RecipeControllerTest
+    {
+
+        [Fact]
+        public void RecipeIndex_ShouldReturnCorrectViewModelAndViewResult()
+        {
+
+            //Arrange
+
+            using var data = DatabaseMock.Instance;
+            var recipeService = new RecipeService(data);
+            var recipeController = new RecipeController(recipeService);
+            
+            var recipe = new Recipe()
+            {
+                Id = 1,
+                Description = "description",
+                ImageUrl = "imageurl",
+                Name = "pizza margarita"
+            };
+
+            data.Recipes.Add(recipe);
+            data.SaveChanges();
+
+            //Act
+
+            var currentRecipes = new List<RecipeViewModel>();
+
+            var viewResult = recipeController.Index(currentRecipes);
+
+            //Assert
+
+            var viewModel = Assert.IsType<ViewResult>(viewResult);
+
+            var listOfRecipes = Assert.IsType<List<RecipeViewModel>>(viewModel.Model);
+
+            Assert.Equal(1, listOfRecipes.Count);
+   
+        }
+
+    }
+}
