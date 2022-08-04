3647d628b90ef3d9353a61d62283999c5ac74df9
rbbozhilov <rbbojilov@gmail.com>
2022-08-04 23:10:39 +0300

    Refactoring

--- LionSkyNot/LionSkyNot/Infrastructure/AttributeValidations/CardNumberAttribute.cs
+++ LionSkyNot/LionSkyNot/Infrastructure/AttributeValidations/CardNumberAttribute.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using LionSkyNot.Models.Algorithms;
+using System.ComponentModel.DataAnnotations;
 using System.Text.RegularExpressions;
 
 namespace LionSkyNot.Infrastructure.AttributeValidations
@@ -12,53 +13,7 @@ namespace LionSkyNot.Infrastructure.AttributeValidations
 
             if (value is string cardNumber)
             {
-                cardNumber = Regex.Replace(cardNumber, @"\s+", "");
-
-                if (!double.TryParse(cardNumber, out double result))
-                {
-                    return false;
-                }
-
-                if (cardNumber.Length <= 1)
-                {
-                    return false;
-                }
-
-                var finalCreditCardNumbers = new List<int>();
-                int counter = 0;
-                int indexer = cardNumber.Length - 1;
-
-                for (int i = cardNumber.Length; i > 0; i--)
-                {
-                    counter++;
-
-                    if (counter % 2 == 0)
-                    {
-                        string doubledNumber = ((int)Char.GetNumericValue(cardNumber[indexer]) * 2).ToString();
-
-                        if (doubledNumber.Length > 1)
-                        {
-                            int sumOfTwoDigits = 0;
-                            sumOfTwoDigits = (int)Char.GetNumericValue(doubledNumber[0]) + (int)Char.GetNumericValue(doubledNumber[1]);
-                            finalCreditCardNumbers.Add(sumOfTwoDigits);
-                        }
-                        else
-                        {
-                            finalCreditCardNumbers.Add(int.Parse(doubledNumber));
-                        }
-                    }
-                    else
-                    {
-                        finalCreditCardNumbers.Add((int)Char.GetNumericValue(cardNumber[indexer]));
-                    }
-
-                    indexer--;
-                }
-
-                if (finalCreditCardNumbers.Sum() % 10 == 0)
-                {
-                    isValid = true;
-                }
+                isValid = new LuhnAlgorithm().Implementation(cardNumber);
 
             }
 
---
+++ LionSkyNot/LionSkyNot/Models/Algorithms/LuhnAlgorithm.cs
@@ -0,0 +1,63 @@
+﻿using System.Text.RegularExpressions;
+
+namespace LionSkyNot.Models.Algorithms
+{
+    public class LuhnAlgorithm
+    {
+
+        public bool Implementation(string cardNumber)
+        {
+            bool isvalid = false;
+            cardNumber = Regex.Replace(cardNumber, @"\s+", "");
+
+            if (!double.TryParse(cardNumber, out double result))
+            {
+                return false;
+            }
+
+            if (cardNumber.Length <= 1)
+            {
+                return false;
+            }
+
+            var finalCreditCardNumbers = new List<int>();
+            int counter = 0;
+            int indexer = cardNumber.Length - 1;
+
+            for (int i = cardNumber.Length; i > 0; i--)
+            {
+                counter++;
+
+                if (counter % 2 == 0)
+                {
+                    string doubledNumber = ((int)Char.GetNumericValue(cardNumber[indexer]) * 2).ToString();
+
+                    if (doubledNumber.Length > 1)
+                    {
+                        int sumOfTwoDigits = 0;
+                        sumOfTwoDigits = (int)Char.GetNumericValue(doubledNumber[0]) + (int)Char.GetNumericValue(doubledNumber[1]);
+                        finalCreditCardNumbers.Add(sumOfTwoDigits);
+                    }
+                    else
+                    {
+                        finalCreditCardNumbers.Add(int.Parse(doubledNumber));
+                    }
+                }
+                else
+                {
+                    finalCreditCardNumbers.Add((int)Char.GetNumericValue(cardNumber[indexer]));
+                }
+
+                indexer--;
+            }
+
+            if (finalCreditCardNumbers.Sum() % 10 == 0)
+            {
+                isvalid = true;
+            }
+
+            return isvalid;
+        }
+
+    }
+}
