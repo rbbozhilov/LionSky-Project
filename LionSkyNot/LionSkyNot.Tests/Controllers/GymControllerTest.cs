b1bec08f36a5096dfd66448f743605b06f33bba6
rbbozhilov <rbbojilov@gmail.com>
2022-08-08 21:13:10 +0300

    Add integration test for gym controller

---
+++ LionSkyNot/LionSkyNot.Tests/Controllers/GymControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using LionSkyNot.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Xunit;
+
+namespace LionSkyNot.Tests.Controllers
+{
+    public class GymControllerTest
+    {
+
+        [Fact]
+        public void IndexOfGymController_ShouldReturnViewResult()
+        {
+            //Arrange
+
+            var gymController = new GymController();
+
+            //Act
+
+            var result = gymController.Index();
+
+            //Assert
+            Assert.IsType<ViewResult>(result);
+        }
+
+    }
+}
