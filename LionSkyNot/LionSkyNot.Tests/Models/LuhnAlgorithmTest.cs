e37310fc78872a86e08bb64b4b0fd3d1e4577d63
rbbozhilov <rbbojilov@gmail.com>
2022-11-23 22:46:09 +0200

    Add some unit tests for Luhn Algorithm

---
+++ LionSkyNot/LionSkyNot.Tests/Models/LuhnAlgorithmTest.cs
@@ -0,0 +1,62 @@
+﻿using LionSkyNot.Models.Algorithms;
+using System.Collections.Generic;
+
+using Xunit;
+
+namespace LionSkyNot.Tests.Models
+{
+    public class LuhnAlgorithmTest
+    {
+
+
+        [Fact]
+        public void Implementation_ShouldReturnFalse_TheNumberIsText()
+        {
+            //Arrange
+
+            LuhnAlgorithm algorithm = new LuhnAlgorithm();
+            string cardNumber = "Test";
+
+            //Act & Assert
+
+            Assert.False(algorithm.Implementation(cardNumber));
+        }
+
+
+        [Fact]
+        public void Implementation_ShouldReturnFalse_TheNumberIsLessThenTwo()
+        {
+            //Arrange
+
+            LuhnAlgorithm algorithm = new LuhnAlgorithm();
+            string cardNumber = "1";
+
+            //Act & Assert
+            Assert.False(algorithm.Implementation(cardNumber));
+        }
+
+        [Fact]
+        public void Implementation_ShouldReturnFalse_TheNumberIsNotReal()
+        {
+            //Arrange
+
+            LuhnAlgorithm algorithm = new LuhnAlgorithm();
+            string cardNumber = "4999999999999108";
+
+            //Act & Assert
+            Assert.False(algorithm.Implementation(cardNumber));
+        }
+
+        [Fact]
+        public void Implementation_ShouldReturnTrue_TheNumberIsCorrect()
+        {
+            //Arrange
+
+            LuhnAlgorithm algorithm = new LuhnAlgorithm();
+            string cardNumber = "424 242 42 42 42 4242";
+
+            //Act & Assert
+            Assert.True(algorithm.Implementation(cardNumber));
+        }
+    }
+}
--- LionSkyNot/LionSkyNot/Models/Algorithms/LuhnAlgorithm.cs
+++ LionSkyNot/LionSkyNot/Models/Algorithms/LuhnAlgorithm.cs
@@ -58,6 +58,5 @@ namespace LionSkyNot.Models.Algorithms
 
             return isValid;
         }
-
     }
 }
